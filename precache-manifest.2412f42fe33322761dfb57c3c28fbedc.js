2159b2ba5f84506e833325d96ae1395cdc716de4
Vitalii Arseniuk <arsenyuk.v.v@gmail.com>
2020-10-04 13:00:43 +0300

    Updates

---
+++ precache-manifest.2412f42fe33322761dfb57c3c28fbedc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f62626a6912aa2f43cf94abeca2c18e2",
+    "url": "/cra_base/index.html"
+  },
+  {
+    "revision": "307e9cab7ef9bd9a256c",
+    "url": "/cra_base/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2d9cf448e24370ca13ca",
+    "url": "/cra_base/static/js/2.02c77a97.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cra_base/static/js/2.02c77a97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "307e9cab7ef9bd9a256c",
+    "url": "/cra_base/static/js/main.fe5610cb.chunk.js"
+  },
+  {
+    "revision": "3eb07e94ba3fb7e0a299",
+    "url": "/cra_base/static/js/runtime-main.90a7062b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cra_base/static/media/logo.5d5d9eef.svg"
+  }
+]);
